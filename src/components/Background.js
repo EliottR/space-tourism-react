70e5f959bbf596385bb4a2c5dc7cefe8027d794b
Eliott Raheriarisoa <eliott.raheriarisoa@gmail.com>
2021-12-20 13:59:51 +0100

    [modif] theme color

--- src/components/Background.js
+++ src/components/Background.js
@@ -12,26 +12,31 @@ const Background = () => {
         case '/':
             document.body.classList = '';
             document.body.classList.add('bg-home');
+            document.querySelector('meta[name="theme-color"]').setAttribute("content", '#0B0D19');
             break;
 
         case '/destination':
             document.body.classList = '';
             document.body.classList.add('bg-destination');
+            document.querySelector('meta[name="theme-color"]').setAttribute("content", '#FF2600');
             break;
 
         case '/crew':
             document.body.classList = '';
             document.body.classList.add('bg-crew');
+            document.querySelector('meta[name="theme-color"]').setAttribute("content", '#27282D');
             break;
 
         case '/technology':
             document.body.classList = '';
             document.body.classList.add('bg-technology');
+            document.querySelector('meta[name="theme-color"]').setAttribute("content", '#27282D');
             break;
 
         default:
             document.body.classList = '';
             document.body.classList.add('bg-home');
+            document.querySelector('meta[name="theme-color"]').setAttribute("content", '#FF2600');
             break;
     }
 
