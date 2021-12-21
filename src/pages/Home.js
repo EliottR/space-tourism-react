e98a8937c63ff7d1a3d5a212eabc1978f5365461
Eliott Raheriarisoa <eliott.raheriarisoa@gmail.com>
2021-12-21 22:58:51 +0100

    [modif] gsap animation reveal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,31 @@
 import gsap from 'gsap';
 import Header from '../components/Header';
 import { useNavigate } from "react-router-dom";
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import { Power4 } from 'gsap';
 
 
 const Home = () => {
 
     let navigate = useNavigate();
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    let revealTl = gsap.timeline();
+    let reveal = gsap.utils.toArray('.gs_reveal_fromLeft');
+
+    reveal.forEach((text, i) => {
+        revealTl.fromTo(text, {
+            opacity: 0
+        },
+
+            {
+                opacity: 1,
+                ease: Power4.easeInOut,
+                duration: 0.25
+            }
+        )
+    });
 
     function changePage() {
 
@@ -48,14 +68,14 @@ const Home = () => {
             <main className="home">
 
                 <article className="home__text">
-                    <h2 className="home__text__h2">So, you want to travel to</h2>
-                    <h1 className="home__text__h1">Space</h1>
-                    <h3 className="home__text__h3">Let’s face it; if you want to go to space, you might as well genuinely go to
+                    <h2 className="home__text__h2 gs_reveal_fromLeft">So, you want to travel to</h2>
+                    <h1 className="home__text__h1 gs_reveal_fromLeft">Space</h1>
+                    <h3 className="home__text__h3 gs_reveal_fromLeft">Let’s face it; if you want to go to space, you might as well genuinely go to
                         outer space and not hover kind of on the edge of it. Well sit back, and relax
                         because we’ll give you a truly out of this world experience!</h3>
                 </article>
 
-                <article className="home__circle" onClick={changePage}>
+                <article className="home__circle gs_reveal_fromLeft" onClick={changePage}>
                     <p className="home__circle__text">Explore</p>
                 </article>
 
