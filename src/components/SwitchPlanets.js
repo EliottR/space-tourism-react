934feb755e6bd0429e1f904f80954b1b787d874b
Eliott Raheriarisoa <eliott.raheriarisoa@gmail.com>
2021-12-22 13:14:54 +0100

    [modif] destination

--- src/components/SwitchPlanets.js
+++ src/components/SwitchPlanets.js
@@ -2,13 +2,31 @@ import React from 'react';
 
 const SwitchPlanets = () => {
 
+    function removeClass(e) {
+        for (let i = 0; i < e.target.parentNode.childNodes.length; i++) {
+            if (i != e.target.attributes.index.value) {
+                e.target.parentNode.childNodes[i].classList.remove('activePlanets');
+            }
+        }
+    }
+
     function changePlanet(e) {
-        console.log(e)
-
-        switch (e.target.attributes.index.value) {
+        switch (parseInt(e.target.attributes.index.value)) {
             case 0:
-
-                e.target.classList.add('active')
+                removeClass(e);
+                e.target.classList.add('activePlanets')
+                break;
+            case 1:
+                removeClass(e);
+                e.target.classList.add('activePlanets')
+                break;
+            case 2:
+                removeClass(e);
+                e.target.classList.add('activePlanets')
+                break;
+            case 3:
+                removeClass(e);
+                e.target.classList.add('activePlanets')
                 break;
         }
     }
@@ -16,7 +34,7 @@ const SwitchPlanets = () => {
     let index = 0;
     return (
         <ul className='destination__switchPlanets'>
-            <li index={index++} onClick={changePlanet} className="destination__switchPlanets__tab">moon</li>
+            <li index={index++} onClick={changePlanet} className="destination__switchPlanets__tab activePlanets">moon</li>
             <li index={index++} onClick={changePlanet} className="destination__switchPlanets__tab">mars</li>
             <li index={index++} onClick={changePlanet} id="test" className="destination__switchPlanets__tab">europa</li>
             <li index={index++} onClick={changePlanet} className="destination__switchPlanets__tab">titan</li>
--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -2,15 +2,22 @@ import React from 'react';
 import Header from '../components/Header';
 import NamePage from '../components/NamePage';
 import Planets from '../components/Planets';
-import logo from '../assets/destination/image-moon.png';
 
+import logo from '../assets/destination/image-moon.png';
+import data from '../data/data.json';
 
 const Destination = () => {
+    const obj = JSON.stringify(data);
+    const json = JSON.parse(obj);
+
     return (
         <>
             <Header />
             <main className='destination'>
                 <NamePage index='01' text='pick your destination' />
+
+                {/* <h1>{json.destinations[0].name}</h1> */}
+
                 <Planets img={logo} name='moon' description='See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.' />
             </main>
         </>
